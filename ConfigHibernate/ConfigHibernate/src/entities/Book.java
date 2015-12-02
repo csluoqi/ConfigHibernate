7d10d09efbffbd656a8179070abb753e2ffa673a
csluoqi <csluoqi@126.com>
2015-12-02 22:31:54 +0800

    [提交单号]:N/A [提交描述]: 1.学习了session的flush方法 [提交人员]:lwx315503 [提交时间]:2015.12.02

--- ConfigHibernate/ConfigHibernate/src/entities/Book.java
+++ ConfigHibernate/ConfigHibernate/src/entities/Book.java
@@ -88,6 +88,72 @@ public class Book
         this.publishDate = publishDate;
     }
 
+    
+    
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+        result = prime * result + ((Name == null) ? 0 : Name.hashCode());
+        result = prime * result + ((Price == null) ? 0 : Price.hashCode());
+        result = prime * result + ((author == null) ? 0 : author.hashCode());
+        result = prime * result + ((info == null) ? 0 : info.hashCode());
+        result = prime * result
+                + ((publishDate == null) ? 0 : publishDate.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Book other = (Book) obj;
+        if (Id == null)
+        {
+            if (other.Id != null)
+                return false;
+        } else if (!Id.equals(other.Id))
+            return false;
+        if (Name == null)
+        {
+            if (other.Name != null)
+                return false;
+        } else if (!Name.equals(other.Name))
+            return false;
+        if (Price == null)
+        {
+            if (other.Price != null)
+                return false;
+        } else if (!Price.equals(other.Price))
+            return false;
+        if (author == null)
+        {
+            if (other.author != null)
+                return false;
+        } else if (!author.equals(other.author))
+            return false;
+        if (info == null)
+        {
+            if (other.info != null)
+                return false;
+        } else if (!info.equals(other.info))
+            return false;
+        if (publishDate == null)
+        {
+            if (other.publishDate != null)
+                return false;
+        } else if (!publishDate.equals(other.publishDate))
+            return false;
+        return true;
+    }
+
     @Override
     public String toString()
     {
